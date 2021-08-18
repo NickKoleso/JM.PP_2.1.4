6f54e83e4b8389a22e71d5801097befa86c802c4
NickKoleso <nick.kolesnichenko@gmail.com>
2021-08-18 18:55:14 +0300

    Решение задачи 2.1.4

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private final Deth8 deth;
+
+    public Needle7(Deth8 death) {
+        this.deth = death;
+    }
+
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
